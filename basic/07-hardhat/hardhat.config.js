b36a4024c53651daa0f27c1ad5260124401ef506
liuzl <jueshancoder@gmail.com>
2022-11-04 11:53:15 +0800

    finish basic-07 use hardhat to test and deploy contract.

--- basic/07-hardhat/hardhat.config.js
+++ basic/07-hardhat/hardhat.config.js
@@ -1,13 +1,13 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
-task('accounts', 'Prints the list of accounts', async() => {
-  const accounts = await ethers.getSigner();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+// task('accounts', 'Prints the list of accounts', async() => {
+//   const accounts = await ethers.getSigner();
+//
+//   for (const account of accounts) {
+//     console.log(`accounts is ${account.address}`);
+//   }
+// });
 
 function mnemonic() {
   console.log(process.env.PRIVATE_KEY);
@@ -22,8 +22,8 @@ module.exports = {
     localhost: {
       url: "http://localhost:8545",
     },
-    goerli: {
-      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_ID, //<---- CONFIG YOUR INFURA ID IN .ENV! (or it won't work)
+    sepolia: {
+      url: 'https://sepolia.infura.io/v3/' + process.env.INFURA_ID, //<---- CONFIG YOUR INFURA ID IN .ENV! (or it won't work)
       accounts: [mnemonic()],
     },
   },
--- basic/07-hardhat/test/erc20.test.js
+++ basic/07-hardhat/test/erc20.test.js
@@ -1,21 +1,21 @@
 const { expect } = require("chai");
 
 describe("Token contract", function () {
-    it('Deployment should assign the total supply of tokens to the owner', async function () {
-        const [owner] = await ethers.getSigners();
-        console.log(owner.address);
-        const Token = await ethers.getContractFactory("SimpleToken");
-        const hardhatToken = await Token.deploy("HEHE", "HH", 1, 100000000);
-
-        const ownerBalance = await hardhatToken.balanceOf(owner.address);
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-    });
+    // it('Deployment should assign the total supply of tokens to the owner', async function () {
+    //     const [owner] = await ethers.getSigners();
+    //     console.log(owner.address);
+    //     const Token = await ethers.getContractFactory("SimpleToken");
+    //     const hardhatToken = await Token.deploy("HEHE", "HH", 1, 100000000);
+    //
+    //     const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    //     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    // });
 
     it('should transfer tokens between accounts', async function () {
-        const [owner, add1, add2] = await ethers.getSigners();
+        const [owner, addr1, addr2] = await ethers.getSigners();
         console.log(owner.address);
-        console.log(add1.address);
-        console.log(add2.address);
+        console.log(addr1.address);
+        console.log(addr2.address);
 
         const Token = await ethers.getContractFactory("SimpleToken");
 
