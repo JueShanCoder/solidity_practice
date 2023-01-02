27333ac02360e95f2c7d500d86058fb7ad881da4
jueshan <jueshancoder@gmail.com>
2023-01-02 15:17:23 +0800

    added 15-nft-blindbox-chainlink-vrf code.

--- basic/15-nft-blindbox-chainlink-vrf/contracts/DungeonsAndDragonsCharacter.sol
+++ basic/15-nft-blindbox-chainlink-vrf/contracts/DungeonsAndDragonsCharacter.sol
@@ -2,7 +2,99 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
 
-contract DungeonsAndDragonsCharacter is ERC721PresetMinterPauserAutoId, VRFConsumerBaseV2{
+
+contract DungeonsAndDragonsCharacter is ERC721PresetMinterPauserAutoId, VRFConsumerBaseV2 {
+    using Strings for string;
+
+    event FulfillRandomness(uint256, uint256[]);
+    event RequestId(address, uint256);
+
+    VRFCoordinatorV2Interface COORDINATOR;
+    LinkTokenInterface LINKTOKEN;
+
+    uint64 s_subscriptionId;
+
+    // Goerli coordinator. For other networks,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+
+    // Goerli LINK token contract. For other networks,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    address link = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords = 2;
+
+    bool public flag;
+    uint256 public tag;
+
+    struct Character {
+        uint256 strength;
+        uint256 dexterity;
+        uint256 constitution;
+        uint256 intelligence;
+        uint256 wisdom;
+        uint256 charisma;
+        uint256 experience;
+        string name;
+    }
+
+    Character[] public characters;
+
+    mapping(uint256 => string)  requestToCharacterName;
+    mapping(uint256 => address) requestToSender;
+    mapping(uint256 => uint256) requestToRandomNum;
+
+    constructor(uint64 subscriptionId, string memory tokenURI) public VRFConsumerBaseV2(vrfCoordinator)
+    ERC721PresetMinterPauserAutoId('DungeonsAndDragonsCharacter', "D&D", tokenURI) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        LINKTOKEN = LinkTokenInterface(link);
+        s_subscriptionId = subscriptionId;
+    }
+
+    function requestNewRandomCharacter(string memory name) public returns (uint256) {
+        uint256 requestId = COORDINATOR.requestRandomWords(keyHash, s_subscriptionId, requestConfirmations, callbackGasLimit, numWords);
+        emit RequestId(msg.sender, requestId);
+
+        requestToCharacterName[requestId] = name;
+        requestToSender[requestId] = msg.sender;
+        return requestId;
+    }
+
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved"
+        );
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        requestToRandomNum[requestId] = randomWords[0];
+        emit FulfillRandomness(requestId, randomWords);
+    }
+
 
 }
